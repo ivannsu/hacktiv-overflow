8e4698587fe5b8bfb6799f4dba1811a0197ec45a
ivan <ivansu.webmail@gmail.com>
2018-09-23 21:21:34 +0700

    create crypt helper

---
+++ server/helpers/crypt.js
@@ -0,0 +1,7 @@
+module.exports = (password) => {
+  const crypto = require('crypto');
+  const hash = crypto.createHmac('sha256', process.env.CRYPT_SECRET_KEY)
+                      .update(password)
+                      .digest('hex');
+  return hash;
+}
