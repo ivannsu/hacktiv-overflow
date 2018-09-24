7c1346645ddee57753fd50013046b6c5c18188b6
ivan <ivansu.webmail@gmail.com>
2018-09-24 18:33:40 +0700

    set token, set user id, remove token, remove user id

--- client/src/store.js
+++ client/src/store.js
@@ -5,12 +5,35 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    token: '',
+    userId: ''
   },
   mutations: {
-
+    setToken (state, payload) {
+      state.token = payload
+    },
+    setUserId (state, payload) {
+      state.userId = payload
+    },
+    removeToken (state) {
+      state.token = ''
+    },
+    removeUserId (state) {
+      state.userId = ''
+    }
   },
   actions: {
-
+    commitToken ({ commit }, payload) {
+      commit('setToken', payload)
+    },
+    commitUserId ({ commit }, payload) {
+      commit('setUserId', payload)
+    },
+    removeToken ({ commit }) {
+      commit('removeToken')
+    },
+    removeUserId ({ commit }) {
+      commit('removeUserId')
+    }
   }
 })
