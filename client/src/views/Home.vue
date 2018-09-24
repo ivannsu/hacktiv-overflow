ce397b790db37ed31fbd9067fd9cf4653db91ff7
ivan <ivansu.webmail@gmail.com>
2018-09-24 19:13:45 +0700

    home template

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="col-lg-8">
+    <div class="row">
+
+      <div class="col-lg-6">
+        <div class="card">
+          <div class="card-body">
+            <h4><router-link to="/" class="text-dark"><strong>Some title</strong></router-link></h4>
+            <blockquote class="blockquote mb-0">
+              <footer class="blockquote-footer">Author: <em>Ivan</em></footer>
+            </blockquote>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
+  created () {
+    axios({
+      method: 'GET',
+      
+    })
   }
 }
 </script>
