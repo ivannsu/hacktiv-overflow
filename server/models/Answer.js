d1ca28e7d015456d7213a68c3c48bd01ec658042
ivan <ivansu.webmail@gmail.com>
2018-09-24 11:41:14 +0700

    back to this answer schema

--- server/models/Answer.js
+++ server/models/Answer.js
@@ -2,19 +2,13 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const answerSchema = new Schema({
-  question: {
-    type: Schema.Types.ObjectId,
-    ref: 'Question'
+  answer: {
+    type: String
   },
-  answers: [
-    {
-      answer: { type: String },
-      user: {
-        type: Schema.Types.ObjectId,
-        ref: 'User'
-      }
-    }
-  ],
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }
 })
 
 const Answer = mongoose.model('Answer', answerSchema)
