1bf1c84b605329b085adb496960ad8a503f886f7
ivan <ivansu.webmail@gmail.com>
2018-09-24 16:04:44 +0700

    import facebook sign in button package

--- client/src/main.js
+++ client/src/main.js
@@ -2,8 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import FBSignInButton from 'vue-facebook-signin-button'
 
 Vue.config.productionTip = false
+
+Vue.use(FBSignInButton)
+Vue.prototype.$baseurl = 'http://localhost:3000'
 
 new Vue({
   router,
