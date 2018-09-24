75b32d21371fe8004f67619762f8ebe34eb80691
ivan <ivansu.webmail@gmail.com>
2018-09-24 19:14:05 +0700

    add sidebar and navbar component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,16 +1,23 @@
 <template>
   <div>
     <Navbar/>
-    <router-view/>
+    <div class="container-fluid" style="padding-top: 20px">
+      <div class="row">
+        <Sidebar/>
+        <router-view/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Sidebar from './components/Sidebar.vue'
 import Navbar from './components/Navbar.vue'
 
 export default {
   components: {
-    Navbar
+    Navbar,
+    Sidebar
   }
 }
 </script>
