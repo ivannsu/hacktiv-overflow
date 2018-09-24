40e330b5e6d5f86d9102a057c848d52e48f5d28b
ivan <ivansu.webmail@gmail.com>
2018-09-24 19:11:12 +0700

    sidebar component

---
+++ client/src/components/Sidebar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="col-lg-3">
+    <ul class="list-group">
+      <li class="list-group-item text-info">Cras justo odio</li>
+      <li class="list-group-item text-info">Dapibus ac facilisis in</li>
+      <li class="list-group-item text-info">Morbi leo risus</li>
+      <li class="list-group-item text-info">Porta ac consectetur ac</li>
+      <li class="list-group-item text-info">Vestibulum at eros</li>
+    </ul>
+  </div>  
+</template>
+
+<script>
+
+export default {
+
+}
+
+</script>
+
+<style>
+
+</style>
