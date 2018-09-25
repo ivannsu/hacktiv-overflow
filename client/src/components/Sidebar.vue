9b81d5415a91c6871cfb762576780aead871e1c6
ivan <ivansu.webmail@gmail.com>
2018-09-25 16:30:51 +0700

    update sidebar link

--- client/src/components/Sidebar.vue
+++ client/src/components/Sidebar.vue
@@ -2,7 +2,8 @@
   <div class="col-lg-3">
     <ul class="list-group">
       <router-link v-if="token" :to="{ name: 'create-question' }" class="list-group-item list-group-item-success text-ligth"><span class="fa fa-plus"></span> Ask Question</router-link>
-      <router-link to="/" class="list-group-item list-group-item-light"><span class="fa fa-users"></span> Users</router-link>
+      <router-link :to="{ name: 'all-question' }" class="list-group-item list-group-item-light"><span class="fa fa-book"></span> Questions</router-link>
+      <router-link :to="{ name: 'all-user' }" class="list-group-item list-group-item-light"><span class="fa fa-users"></span> Users</router-link>
     </ul>
   </div>
 </template>
