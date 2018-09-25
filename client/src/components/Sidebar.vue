eb14a4f67a0127a4df24935dd45bce516e802730
ivan <ivansu.webmail@gmail.com>
2018-09-25 12:10:38 +0700

    props token and add condition to create button

--- client/src/components/Sidebar.vue
+++ client/src/components/Sidebar.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="col-lg-3">
     <ul class="list-group">
-      <li class="list-group-item text-info"><router-link to="/"><span class="fa fa-users"></span> User</router-link></li>
+      <router-link v-if="token" :to="{ name: 'create-question' }" class="list-group-item list-group-item-success text-ligth"><span class="fa fa-plus"></span> Ask Question</router-link>
+      <router-link to="/" class="list-group-item list-group-item-light"><span class="fa fa-users"></span> Users</router-link>
     </ul>
   </div>
 </template>
@@ -9,7 +10,12 @@
 <script>
 
 export default {
-
+  name: 'Sidebar',
+  computed: {
+    token () {
+      return this.$store.state.token
+    }
+  }
 }
 
 </script>
