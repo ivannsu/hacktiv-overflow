936139e20fb13608d8a57642fcf102bc1cefa65d
ivan <ivansu.webmail@gmail.com>
2018-09-26 15:53:12 +0700

    init wyswig

--- client/src/main.js
+++ client/src/main.js
@@ -3,10 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import FBSignInButton from 'vue-facebook-signin-button'
+import wysiwyg from "vue-wysiwyg";
 
 Vue.config.productionTip = false
 
 Vue.use(FBSignInButton)
+Vue.use(wysiwyg, {
+  minheight: '500px'
+});
 Vue.prototype.$baseurl = 'http://localhost:3000'
 
 new Vue({
